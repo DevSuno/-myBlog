7e4f7a853ec5f7d27c8b5ebacfb6034d8eb5cf0b
Suno <dev.suno01@gmail.com>
2020-07-17 21:47:08 +0800

    对src/api/auth做封装

---
+++ src/pages/api/blog.js
@@ -0,0 +1,15 @@
+import request from "../../helpers/request";
+
+const URL = {
+  GET_LIST: '/blog',
+  GET_DETAIL: '/blog/:blogid',
+  CREATE: '/blog',
+  UPDATE: '/blog/:blogid',
+  DELETE: '/blog/:blogid'
+}
+
+export default {
+  getBlogs({}){
+
+  }
+}
